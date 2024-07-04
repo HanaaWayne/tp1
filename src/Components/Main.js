47bf0d47e735fe44aae46996e7c87ce590802b8c
hanaachouiter7@gmail.com <hanaachouiter7@gmail.com>
2024-07-04 15:14:53 +0200

    modif const day  main

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -29,7 +29,8 @@ export default function Main(){
         border-radius: 10px
     `
 
-    const day = [1,2,3,4,5,6,7,8,,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+    // const day = [1,2,3,4,5,6,7,8,,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+    const day = Array.from({ length: 30 }, (_, i) => i + 1);
     const month = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
     // const year = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]
 
